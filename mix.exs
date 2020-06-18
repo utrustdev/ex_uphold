c2829b66dc6fdd6152ec22363c0bb031a5c6004d
João Rodrigues <joao.rod94@gmail.com>
2020-06-18 19:50:41 +0100

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExUphold.MixProject do
   def project do
     [
       app: :ex_uphold,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.8",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env() == :prod,
